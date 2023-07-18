3f0547d7d3c0a3a8bd39c82094c336bbfb9e347a
lpjhelder <lpjhelder@gmail.com>
2023-07-17 23:23:11 -0300

    modulo jose

--- scripts/check_modules.py
+++ scripts/check_modules.py
@@ -24,10 +24,12 @@ modules_to_check = {
     "uvicorn": "0.19.0",
     "fastapi": "0.85.1",
     "pycloudflared": "0.2.0",
-    "passlib": "1.7.4"
+    "passlib": "1.7.4",
+    "python-jose[cryptography]": "3.3.0"
     # "xformers": "0.0.16",
 }
-modules_to_log = ["torch", "torchvision", "sdkit", "stable-diffusion-sdkit", "passlib"]
+modules_to_log = ["torch", "torchvision", "sdkit", "stable-diffusion-sdkit", "passlib", "python-jose[cryptography]"] 
+
 
 
 
